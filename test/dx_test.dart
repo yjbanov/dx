c71b4584d8635893da8330c736a5d6412f70d1f5
Yegor Jbanov <yjbanov@google.com>
2017-09-26 20:38:05 -0700

    a project to start with

---
+++ test/dx_test.dart
@@ -0,0 +1,14 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:dx/dx.dart' as dx;
+import 'package:test/test.dart';
+
+void main() {
+  group('main', () {
+    test('works', () {
+      dx.main();
+    });
+  });
+}
